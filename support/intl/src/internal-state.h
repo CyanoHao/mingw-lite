0b589c5ba8c483f0c1b8a9b0fc5d8d0b28f1cb30
Cyano Hao <c@cyano.cn>
2025-06-30 11:02:21 +0800

    [ci skip] make libintl less tricky

--- support/intl/src/internal-state.h
+++ support/intl/src/internal-state.h
@@ -1,11 +1,10 @@
 #pragma once
 
-#include <memory>
-
-#include <stdlib.h>
-#include <string>
+#include "eastl.h"
 
 namespace intl
 {
-  inline std::string default_domain;
+  using string = eastl::basic_string<char, eastl::allocator_malloc>;
+
+  extern string default_domain;
 } // namespace intl
