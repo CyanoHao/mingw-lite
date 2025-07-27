986fcfadca6f2b80624cc830d493d86e85da0a93
Cyano Hao <c@cyano.cn>
2025-07-27 22:59:03 +0800

    [ci skip] save

--- support/nostl/include/nostl/vector.h
+++ support/nostl/include/nostl/vector.h
@@ -186,28 +186,67 @@ namespace NS_NOSTL
       DoSwap(x);
     }
 
-    vector(this_type &&x, const allocator_type &allocator);
-    vector(std::initializer_list<value_type> ilist,
-           const allocator_type &allocator = allocator_type());
+    vector(this_type &&x, const allocator_type &allocator)
+        : base_type(allocator)
+    {
+      if (internalAllocator() == x.internalAllocator())
+        DoSwap(x);
+      else {
+        this_type temp(move(*this));
+        temp.swap(x);
+      }
+    }
 
-    // note: this has pre-C++11 semantics:
-    // this constructor is equivalent to the constructor
-    // vector(static_cast<size_type>(first), static_cast<value_type>(last),
-    // allocator) if InputIterator is an integral type.
+    vector(std::initializer_list<value_type> ilist,
+           const allocator_type &allocator = allocator_type())
+        : base_type(allocator)
+    {
+      DoInit(ilist.begin(), ilist.end(), false_type());
+    }
+
     template <typename InputIterator>
     vector(InputIterator first,
            InputIterator last,
-           const allocator_type &allocator = allocator_type());
+           const allocator_type &allocator = allocator_type())
+        : base_type(allocator)
+    {
+      DoInit(first, last, is_integral<InputIterator>());
+    }
 
-    ~vector();
+    ~vector() { destruct(mpBegin, mpEnd); }
 
-    this_type &operator=(const this_type &x);
-    this_type &operator=(std::initializer_list<value_type> ilist);
-    this_type &operator=(
-        this_type &&
-            x); // TODO(c++17):
-                // noexcept(allocator_traits<Allocator>::propagate_on_container_move_assignment::value
-                // || allocator_traits<Allocator>::is_always_equal::value)
+    this_type &operator=(const this_type &x)
+    {
+      if (this != &x) {
+        bool bSlowerPathwayRequired =
+            (internalAllocator() != x.internalAllocator());
+        if (bSlowerPathwayRequired) {
+          DoClearCapacity();
+          internalAllocator() = x.internalAllocator();
+        }
+        DoAssign<const_iterator, false>(x.begin(), x.end(), false_type());
+      }
+      return *this;
+    }
+
+    this_type &operator=(std::initializer_list<value_type> ilist)
+    {
+      using InputIterator =
+          typename std::initializer_list<value_type>::iterator;
+      using IC = typename iterator_traits<InputIterator>::iterator_category;
+      DoAssignFromIterator<InputIterator, false>(
+          ilist.begin(), ilist.end(), IC());
+      return *this;
+    }
+
+    this_type &operator=(this_type &&x)
+    {
+      if (this != &x) {
+        DoClearCapacity();
+        swap(x);
+      }
+      return *this;
+    }
 
     void swap(
         this_type &
@@ -215,66 +254,147 @@ namespace NS_NOSTL
                 // noexcept(allocator_traits<Allocator>::propagate_on_container_move_assignment::value
                 // || allocator_traits<Allocator>::is_always_equal::value)
 
-    void assign(size_type n, const value_type &value);
+    void assign(size_type n, const value_type &value)
+    {
+      DoAssignValues(n, value);
+    }
 
     template <typename InputIterator>
-    void assign(InputIterator first, InputIterator last);
+    void assign(InputIterator first, InputIterator last)
+    {
+      DoAssign<InputIterator, false>(first, last, is_integral<InputIterator>());
+    }
 
-    void assign(std::initializer_list<value_type> ilist);
+    void assign(std::initializer_list<value_type> ilist)
+    {
+      using InputIterator =
+          typename std::initializer_list<value_type>::iterator;
+      using IC = typename iterator_traits<InputIterator>::iterator_category;
+      DoAssignFromIterator<InputIterator, false>(
+          ilist.begin(), ilist.end(), IC());
+    }
 
-    iterator begin() noexcept;
-    const_iterator begin() const noexcept;
-    const_iterator cbegin() const noexcept;
+    iterator begin() noexcept { return mpBegin; }
+    const_iterator begin() const noexcept { return mpBegin; }
+    const_iterator cbegin() const noexcept { return mpBegin; }
 
-    iterator end() noexcept;
-    const_iterator end() const noexcept;
-    const_iterator cend() const noexcept;
+    iterator end() noexcept { return mpEnd; }
+    const_iterator end() const noexcept { return mpEnd; }
+    const_iterator cend() const noexcept { return mpEnd; }
 
-    reverse_iterator rbegin() noexcept;
-    const_reverse_iterator rbegin() const noexcept;
-    const_reverse_iterator crbegin() const noexcept;
+    reverse_iterator rbegin() noexcept { return {mpEnd}; }
+    const_reverse_iterator rbegin() const noexcept { return {mpEnd}; }
+    const_reverse_iterator crbegin() const noexcept { return {mpEnd}; }
 
-    reverse_iterator rend() noexcept;
-    const_reverse_iterator rend() const noexcept;
-    const_reverse_iterator crend() const noexcept;
+    reverse_iterator rend() noexcept { return {mpBegin}; }
+    const_reverse_iterator rend() const noexcept { return {mpBegin}; }
+    const_reverse_iterator crend() const noexcept { return {mpBegin}; }
 
-    bool empty() const noexcept;
-    size_type size() const noexcept;
-    size_type capacity() const noexcept;
+    bool empty() const noexcept { return mpBegin == mpEnd; }
+    size_type size() const noexcept { return size_type(mpEnd - mpBegin); }
+    size_type capacity() const noexcept
+    {
+      return size_type(internalCapacityPtr() - mpBegin);
+    }
 
-    void resize(size_type n, const value_type &value);
-    void resize(size_type n);
-    void reserve(size_type n);
-    void set_capacity(
-        size_type n =
-            base_type::npos); // Revises the capacity to the user-specified
-                              // value. Resizes the container to match the
-                              // capacity if the requested capacity n is less
-                              // than the current size. If n == npos then the
-                              // capacity is reallocated (if necessary) such
-                              // that capacity == size.
-    void shrink_to_fit(); // C++11 function which is the same as set_capacity().
+    void resize(size_type n, const value_type &value)
+    {
+      if (n > size())
+        DoInsertValuesEnd(n - size(), value);
+      else {
+        destruct(mpBegin + n, mpEnd);
+        mpEnd = mpBegin + n;
+      }
+    }
+    void resize(size_type n)
+    {
+      if (n > size())
+        DoInsertValuesEnd(n - size());
+      else {
+        destruct(mpBegin + n, mpEnd);
+        mpEnd = mpBegin + n;
+      }
+    }
+    void reserve(size_type n)
+    {
+      if (n > capacity())
+        DoGrow(n);
+    }
+    void set_capacity(size_type n = base_type::npos)
+    {
+      if (n == npos || n <= size()) {
+        if (n == 0)
+          clear();
+        else if (n < size())
+          resize(n);
+        shrink_to_fit();
+      } else {
+        pointer const pNewData =
+            DoRealloc(n, mpBegin, mpEnd, should_move_tag());
+        destruct(mpBegin, mpEnd);
+        DoFree(mpBegin, (size_type)(internalCapacityPtr() - mpBegin));
 
-    pointer data() noexcept;
-    const_pointer data() const noexcept;
+        const ptrdiff_t nPrevSize = mpEnd - mpBegin;
+        mpBegin = pNewData;
+        mpEnd = pNewData + nPrevSize;
+        internalCapacityPtr() = mpBegin + n;
+      }
+    }
+    void shrink_to_fit()
+    {
+      this_type temp = this_type(move_iterator<iterator>(begin()),
+                                 move_iterator<iterator>(end()),
+                                 internalAllocator());
+      DoSwap(temp);
+    }
 
-    reference operator[](size_type n);
-    const_reference operator[](size_type n) const;
+    pointer data() noexcept { return mpBegin; }
+    const_pointer data() const noexcept { return mpBegin; }
 
-    reference at(size_type n);
-    const_reference at(size_type n) const;
+    reference operator[](size_type n) { return *(mpBegin + n); }
+    const_reference operator[](size_type n) const { return *(mpBegin + n); }
 
-    reference front();
-    const_reference front() const;
+    reference at(size_type n) { return *(mpBegin + n); }
+    const_reference at(size_type n) const { return *(mpBegin + n); }
 
-    reference back();
-    const_reference back() const;
+    reference front() { return *mpBegin; }
+    const_reference front() const { return *mpBegin; }
 
-    void push_back(const value_type &value);
-    reference push_back();
-    void *push_back_uninitialized();
+    reference back() { return *(mpEnd - 1); }
+    const_reference back() const { return *(mpEnd - 1); }
+
+    void push_back(const value_type &value)
+    {
+      if (mpEnd < internalCapacityPtr())
+        ::new (mpEnd++) value_type(value);
+      else
+        DoInsertValueEnd(value);
+    }
+    reference push_back()
+    {
+      if (mpEnd < internalCapacityPtr())
+        ::new (mpEnd++) value_type();
+      else
+        DoInsertValueEnd(value_type());
+
+      return *mpEnd - 1;
+    }
+    void *push_back_uninitialized()
+    {
+      if (mpEnd == internalCapacityPtr()) {
+        const size_type nPrevSize = size_type(mpEnd - mpBegin);
+        const size_type nNewSize = GetNewCapacity(nPrevSize);
+        DoGrow(nNewSize);
+      }
+
+      return mpEnd++;
+    }
     void push_back(value_type &&value);
-    void pop_back();
+    void pop_back()
+    {
+      --mpEnd;
+      mpEnd->~value_type();
+    }
 
     template <class... Args>
     iterator emplace(const_iterator position, Args &&...args);
